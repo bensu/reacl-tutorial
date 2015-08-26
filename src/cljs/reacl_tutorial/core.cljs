f9eee34082852a2813346bd835b1a27640c3bb49
Mike Sperber <sperber@deinprogramm.de>
2015-08-26 16:58:53 +0200

    Contacts + deletion.

--- src/cljs/reacl_tutorial/core.cljs
+++ src/cljs/reacl_tutorial/core.cljs
@@ -24,6 +24,42 @@
   (dom/ul {:class "animals"}
    (map stripe (cycle ["#ff0" "#fff"]) lis)))
 
+(def contacts
+  [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+   {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+   {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+   {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+   {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+   {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}])
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name
+  [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(reacl/defclass contact-display
+  this contact [parent] ; parent later
+  render
+  (dom/li
+   (dom/span (display-name contact))
+   (dom/button {:onclick (fn [e] (reacl/send-message! parent contact))} "Delete"))) ; add later
+
+(reacl/defclass contacts-display
+  this data []
+  render
+  (dom/div
+   (dom/h2 "Contact list")
+   (dom/ul
+    (map (fn [c] (contact-display c reacl/no-reaction this)) data)))
+  handle-message
+  (fn [msg]
+    (reacl/return :app-state
+                  (vec (remove (fn [c] (= c msg)) data)))))
+           
 ;; string-display
 #_ (def top
   (reacl/render-component
@@ -31,7 +67,12 @@
    string-display "Hello world!" reacl/no-reaction))
 
 ;; list-display
-(def top
+#_(def top
   (reacl/render-component
    (.getElementById js/document "content")
    list-display ["Lion" "Zebra" "Buffalo" "Antelope"] reacl/no-reaction))
+
+(def top
+  (reacl/render-component
+   (.getElementById js/document "content")
+   contacts-display contacts reacl/no-reaction))
